2b63a304d952773e4ab3c2c0334855e2841c06a2
AmiTh004 <90444032+AmiTh004@users.noreply.github.com>
2022-05-04 10:03:40 +0200

    Initial commit

---
+++ src/main/java/com/example/Hafen_DB/HafenDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hafen_DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HafenDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HafenDbApplication.class, args);
+	}
+
+}
