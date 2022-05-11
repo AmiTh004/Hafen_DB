98ff7d2d855c99a9b56b037d1100536436a5b357
LokiSTR <bent.fenske@gmail.com>
2022-05-11 10:55:18 +0200

    Frischware

---
+++ src/main/java/com/example/Hafen_DB/controller/FrischwareController.java
@@ -0,0 +1,44 @@
+package com.example.Hafen_DB.controller;
+import com.example.Hafen_DB.models.Frischware;
+import com.example.Hafen_DB.controller.FrischwareController;
+import java.util.ArrayList;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class FrischwareController {
+    
+    DBController dbcontroller;
+
+    public ArrayList<Frischware> getAllFrischwaren(){
+
+        // Lokale Frischwaren-Arraylist erstellen
+        ArrayList<Frischware> frischwaren = new ArrayList<>();
+
+        // Das ist DB-Query
+        String sqlSelectAllFrischwaren = "SELECT * FROM `frischware`";
+
+        // Verbindung aufbauen mit USERNAME root und PASSWORT root
+        try{
+            Connection conn = DriverManager.getConnection(dbcontroller.getConnectionUrl(), dbcontroller.getUsername(), dbcontroller.getPasswort()); 
+            PreparedStatement ps = conn.prepareStatement(sqlSelectAllFrischwaren); 
+            ResultSet rs = ps.executeQuery();
+            // Solange es Datensätze in der von der DB angefragen Ressource gibt, werden diese durchgearbeitet und dann als eine ArrayList zurückgegeben
+            while (rs.next()) {
+                int id = (int) rs.getLong("id");
+                String frischware_name = rs.getString("frischware_name");
+                frischwaren.add(new Frischware(id, frischware_name));
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
+
+        return frischwaren;
+    }
+
+
+}
--- src/main/java/com/example/Hafen_DB/models/Frischware.java
+++ src/main/java/com/example/Hafen_DB/models/Frischware.java
@@ -1,5 +1,32 @@
 package com.example.Hafen_DB.models;
 
 public class Frischware {
+ 
+    int id;
+    String frischware_name;
+
+    public Frischware(int id, String frischware_name){
+        setId(id);
+        setFrischware_name(frischware_name);
+    }
+
     
+    /**
+     * 
+     * 
+     * SETTER UND GETTER
+     */
+    
+    public String getFrischware_name() {
+        return frischware_name;
+    }
+    public void setFrischware_name(String frischware_name) {
+        this.frischware_name = frischware_name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 }
